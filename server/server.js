da130f362822e9485b0f80a3c6e7079094e2023c
dkingbrandt <dkingbrandt@hotmail.com>
2023-05-17 22:20:45 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+
+
+app.listen(8080, () => {
+  console.log("Server started on Port ");
+});
