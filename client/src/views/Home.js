4447ca5460a921a593176637b2bb648be6364f49
BexRyd <becka_91_@hotmail.com>
2023-05-18 21:22:19 +0100

    navbar and homepage

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import './../scss/pages/home.scss';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='TopContainer'>
+       <Header/>
+      <div className='radialGradientDarkGreen'></div>
+      <div className='radialGradientsmall'></div>
+      <div className='radialGradientbig'></div>
+      <div className='textBox'>
+      <h1 className='textBox-h1'>En Junior webbyrå där idéer blir till verklighet.<br/><span className='textBox-h1-color'>Optimera ditt företag idag!</span></h1>
+       <p className='textBox-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+       <div className='textBox-Btn-Container'>
+        <button className='btn-info'>mer info</button>
+        <button className='btn-contact'>kontakta oss</button>
+       </div>
+      </div>
+    </div>
   )
 }
