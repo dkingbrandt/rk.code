67d01cc32e0d89129a9aad98790ab5478474d409
BexRyd <becka_91_@hotmail.com>
2023-05-20 11:45:58 +0100

    gradient icons and projectcard

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './../scss/pages/home.scss';
 import Header from '../components/Header';
+import data from './../img/data.svg';
+import dollar from './../img/dollar.svg';
+import heart from './../img/heart.svg';
+import comment from './../img/comment.svg';
+import wildline from './../img/wildline.png';
 
 
 export default function Home() {
@@ -24,8 +29,57 @@ export default function Home() {
 
     <div  className='middleContainer'>
     <hr/>
-    <div className='middleContainer-img'></div>
+
+    <div className='middleContainer-img'>
+    <div className='middleContainer-grid'>
+
+     <div className='icon-container'>
+      <img className='icon data' src={data} alt="computer" />
+      <div>
+         <h4 className='icon-heading'>Skräddarsydd Hemsida</h4> 
+         <p className='icon-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+      </div>
+     </div>
+
+     <div className='icon-container'>
+      <img className='icon dollar' src={dollar} alt="computer" />
+      <div>
+         <h4 className='icon-heading'>Professionell hemsida till ett lågt pris</h4> 
+         <p className='icon-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+      </div>
+     </div>
+
+     <div className='icon-container'>
+      <img className='icon heart' src={heart} alt="computer" />
+      <div>
+         <h4 className='icon-heading'>Passionerade utvecklare</h4> 
+         <p className='icon-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+      </div>
+     </div>
+
+     <div className='icon-container'>
+      <img className='icon comment' src={comment} alt="computer" />
+      <div>
+         <h4 className='icon-heading'>Kontakt med utvecklare/designers</h4> 
+         <p className='icon-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+      </div>
+     </div>
+
+     </div>
+
+    </div>
+
     <hr/>
+
+    <div className='project-container'>
+
+      <div className='card'>
+        <img className='card-img' src={wildline} alt="websitephoto" />
+
+      </div>
+
+    </div>
+
     </div>
 
     </div>
