879709e99d3a7bbd9c1d53d4fdd6bba2c421ab61
dkingbrandt <dkingbrandt@hotmail.com>
2023-06-01 20:20:15 +0200

    validation completed

--- client/src/components/formPriceList.js
+++ client/src/components/formPriceList.js
@@ -11,9 +11,9 @@ import Axios from 'axios';
 
 
 export default function FormPriceList(props) {
-  const [newHeading, setNewHeading] = useState(props.heading)
-  const [newTitle, setNewTitle] = useState(props.title)
-  const [newDescription, setNewDescription] = useState(props.descriptionList)
+  const [newHeading, setNewHeading] = useState("")
+  const [newTitle, setNewTitle] = useState("")
+  const [newDescription, setNewDescription] = useState("")
   const [img, setImg] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [loading, setLoading] = useState(false);
@@ -42,23 +42,43 @@ export default function FormPriceList(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (
-      newTitle.trim() === '' ||
-      !img ||
-      !newDescription ||
-      newHeading.trim() === '' 
-     
-    ) {
-      alert('Field cannot be empty');
-      return;
+    if (props.add) {
+      post("/pricelist/package", {
+        title: newTitle,
+        img: imageUrl,
+        heading: newHeading,
+        description: newDescription,
+
+      })
+        .then(() => get("/pricelist/package/")
+          .then((response) => props.setPricePackage(response.data)))
+        .finally(() => {
+          uploadImage();
+          props.handlePopUp();
+        });
+    } else {
+      put(`/pricelist/package/${props.title}`, {
+        title: newTitle,
+        img: imageUrl,
+        heading: newHeading,
+        description: newDescription,
+
+      })
+        .then(() => get("/pricelist/package/")
+          .then((response) => props.setPricePackage(response.data)))
+        .finally(() => {
+          uploadImage();
+          props.handlePopUp();
+        });
     }
 
+    
   };
 
 
   const handleDescriptionChange = (e) => {
     const descriptionValue = e.target.value;
-    const descriptionArray = descriptionValue.split('\n'); // Split textarea value on line breaks
+    const descriptionArray = descriptionValue.split('\n'); 
 
     setNewDescription(descriptionArray);
   };
@@ -77,13 +97,11 @@ export default function FormPriceList(props) {
           setImg(e.target.files[0]);
         }}
       />
-      {console.log(imageUrl)}
 
       <input
         className="formAbout-heading"
         required
         placeholder={props.title}
-        value={newTitle}
         onChange={(e) => setNewTitle(e.target.value)}
       />
 
@@ -91,7 +109,6 @@ export default function FormPriceList(props) {
         className="formAbout-heading"
         required
         placeholder={props.heading}
-        value={newHeading}
         onChange={(e) => setNewHeading(e.target.value)}
       />
 
@@ -104,42 +121,14 @@ export default function FormPriceList(props) {
 
       <div className='formAbout-btn-container'>
         {props.add &&
-          <button className='formAbout-save-btn' onClick={() => {
-              post("/pricelist/package", {
-                title: newTitle,
-                img: imageUrl,
-                heading: newHeading,
-                description: newDescription,
-
-              })
-                .then(() => get("/pricelist/package/")
-                .then((response) => props.setPricePackage(response.data)))
-                .finally(() => {
-                  uploadImage();
-                  props.handlePopUp();
-                });
-           }} >skapa</button>
+          <button type='submit' className='formAbout-save-btn' o >skapa</button>
         }
 
         {!props.add &&
-          <button className='formAbout-save-btn' onClick={() => {
-            put(`/pricelist/package/${props.title}`, {
-              title: newTitle,
-              img: imageUrl,
-              heading: newHeading,
-              description: newDescription,
-
-            })
-              .then(() => get("/pricelist/package/")
-              .then((response) => props.setPricePackage(response.data)))
-              .finally(() => {
-                uploadImage();
-                props.handlePopUp();
-              });
-          }} >ändra</button>
+          <button type='submit' className='formAbout-save-btn'>ändra</button>
         }
 
-        <button className='formAbout-cancel-btn' onClick={() => props.handlePopUp()}>avbryt</button>
+        <button type='button' className='formAbout-cancel-btn' onClick={() => props.handlePopUp()}>avbryt</button>
       </div>
     </form>
   )
